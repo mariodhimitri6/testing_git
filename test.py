141fad5d022bf531cac4bf4737b099eb80852347
mario <mariodhimitri6@gmail.com>
2025-01-21 11:51:45 +0100

    daje

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ print("Hi my name is Developer prova")
 print("Hello this is newly added line from prova")
 print("Hi my name is Developer main 2")
 print("Hello this is secondly added line")
+
+daje = "Roma"
+if daje == "Roma":
+    print("Daje Roma Daje")
