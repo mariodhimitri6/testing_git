a4e680f9929d88b333cfd196ced5c195a1eea88c
Mario Dhimitri <mario@Marios-MacBook-Pro.local>
2025-01-21 11:50:07 +0100

    val

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-print("Hi my name is Developer prova")
-print("Hello this is newly added line from prova")
-print("Hi my name is Developer main 2")
-print("Hello this is secondly added line")
+val = "Me"
+
+if val != "Me":
+    print("Theo")
+else:
+    print(val)
