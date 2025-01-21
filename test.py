6fb3fc5642b7df9020a7b202ba743b72f8766516
mario <mariodhimitri6@gmail.com>
2025-01-21 15:11:23 +0100

    New code in main

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print("Hi my name is Developer prova")
 print("Hello this is newly added line from prova")
 print("Hi my name is Developer main 2")
+print("New code here")
 print("Hello this is secondly added line")
 
 daje = "Roma"
