960f1321b933d5c79394238998f21cdbab7e40c2
mario <mariodhimitri6@gmail.com>
2025-06-12 18:09:30 +0200

    bugfix rweqwe

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ pdf.ln(10)
 # Function to replace problematic characters with ASCII characters
 def clean_text(text):
     return text.replace("–", "-").replace("’", "'")  # Replace en dash with regular dash and fix apostrophe
-#add 1 comment
+#add 1 commet
 #sdd  comment
 #add 3 fcommena
 # Weekly structure
