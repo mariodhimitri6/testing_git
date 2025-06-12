d8db760a80e668c527b5cc8f290a25dfbff4906e
mario <mariodhimitri6@gmail.com>
2025-06-12 17:46:17 +0200

    new comment 2

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ pdf.ln(10)
 def clean_text(text):
     return text.replace("–", "-").replace("’", "'")  # Replace en dash with regular dash and fix apostrophe
 #add 1 comment
+#sdd 2 comment
 # Weekly structure
 pdf.set_font("Helvetica", size=12)
 pdf.cell(200, 10, txt="Weekly Workout Structure:", ln=True)
