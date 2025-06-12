fbd81ef04c488a4aa92407ef15d8dce6759da78a
mario <mariodhimitri6@gmail.com>
2025-06-12 18:29:11 +0200

    fixed bug number 2

--- test.py
+++ test.py
@@ -16,6 +16,8 @@ def clean_text(text):
 #add 1 commet
 
 print("Bugfix 1")
+print("Bugfix 2")
+
 # Weekly structure
 pdf.set_font("Helvetica", size=12)
 pdf.cell(200, 10, txt="Weekly Workout Structure:", ln=True)
