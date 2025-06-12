cc857742dc567a08308f68c5003bc211d5ce722f
mario <mariodhimitri6@gmail.com>
2025-06-12 17:45:59 +0200

    Added test py pdf

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ pdf.ln(10)
 # Function to replace problematic characters with ASCII characters
 def clean_text(text):
     return text.replace("–", "-").replace("’", "'")  # Replace en dash with regular dash and fix apostrophe
-
+#add 1 comment
 # Weekly structure
 pdf.set_font("Helvetica", size=12)
 pdf.cell(200, 10, txt="Weekly Workout Structure:", ln=True)
